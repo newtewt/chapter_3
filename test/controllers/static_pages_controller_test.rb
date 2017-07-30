dd160ef53c539773d085102a0d0a1978e84f2cdd
Nick Christenson <emailfornc@gmail.com>
2017-07-30 14:56:51 -0400

    complete chapter 5

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,28 +3,29 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @common_title = '| Ruby on Rails Tutorial Sample App'
+    @uncommon_title = 'Ruby on Rails Tutorial Sample App'
   end
   
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
-    assert_select "title", "Home #{@common_title}"
+    assert_select "title", "#{@uncommon_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help #{@common_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About #{@common_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact #{@common_title}"
   end
@@ -32,7 +33,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "route should be home" do
     get root_url
     assert_response :success
-    assert_select "title", "Home #{@common_title}"
+    assert_select "title", "#{@uncommon_title}"
   end
   
 end
